0e2a78309af90b64df133447596c1f743fb015c6
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2024-05-29 20:24:12 +0200

    fix

--- ObserverSubject/Dialog.java
+++ ObserverSubject/Dialog.java
@@ -1,12 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Dialog {
     private static final AddrBook book =
     new AddrBook();
     private static final Store st = new Store();
-    private static final Persona p1 =*
-    new Persona("Oliver", "Stone", "012345", "NY");
+    private static final Persona p1 = new Persona("Oliver", "Stone", "012345", "NY");
     public static void main(String[] args) {
     book.attach(st);
     book.insert(p1);
--- ObserverSubject/Store.java
+++ ObserverSubject/Store.java
@@ -3,16 +3,16 @@ import java.util.List;
 import java.io.*;
 
 public class Store implements Observer {
-    @Override
-    public void update(Subject s, Object o) {
-    List<Persona> l = (List<Persona>) o;
-    String nom;
-    try (FileWriter f = new FileWriter("nomi.txt")) {
-    for (Persona p : l) {
-    nom = p.getNome() + "\t" + p.getCognome() +
-    "\t" + p.getTelefono();
-    f.write(nom + "\n");
-    }
-    } catch (IOException e) { }
-    }
+        @Override
+        public void update(Subject s, Object o) {
+            List<Persona> l = (List<Persona>) o;
+            String nom;
+            try (FileWriter f = new FileWriter("nomi.txt")) {
+                for (Persona p : l) {
+                    nom = p.getNome() + "\t" + p.getCognome() +
+                    "\t" + p.getTelefono();
+                    f.write(nom + "\n");
+                }
+            } catch (IOException e) { }
+        }
     }
